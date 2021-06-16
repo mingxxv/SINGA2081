3d9178c17b288b897f515ab32995a5191205e56e
mingxxv <mingxxv@protonmail.ch>
2021-06-16 12:18:39 +0800

    everything works except death and win

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ $(() => {
             MAIN_ROOM: {
                 title: "Main Room",
                 description: `You hear a faint whirring sound as your VR implant spins up. The room focuses into view. You are in a
-                standard laboratory. There is a locked safe in the centre of the room.`,
+                standard laboratory.git`,
                 actions: [],
             },
             POWER_REACTOR: {
@@ -34,6 +34,22 @@ $(() => {
                 description: `You walk over to the power reactor.\nThe surrounding area is warm, and a faint orange glow emits from the machine.\nThere is a note stuck on the reactor, and a big red button.`,
                 actions: ["POWER_NOTE", "POWER_BUTTON"],
             },
+            STUDY_TABLE: {
+                title: "Study Table",
+                description: `The table is littered with notes and drawings, on paper ("How old!" you wonder).\nThere is an iMike standard-issue computer on the table.`,
+                actions: ["STUDY_NOTES", "STUDY_IMIKE"],
+            },
+            AI_CONSTRUCT: {
+                title: "AI Construct",
+                description: `You see a female AI, quietly meditating in the corner.`,
+                actions: ["AI_TALK"],
+            },
+            CHEMICAL_STORE: {
+                title: "Chemical Store",
+                description: `In the storeroom, there's a small vial left behind on the table.`,
+                actions: ["CHEM_READ", "CHEM_SMELL"],
+            },
+
         },
         actions: {
             POWER_NOTE : {
@@ -43,151 +59,62 @@ $(() => {
             },
             POWER_BUTTON : {
                 title: "Push Button",
-                description: `"Kill Me`,
-            },
-        },
-
-        /*
-        rooms: [
-
-            {
-                id: "MAIN_ROOM",
-                description: `You hear a faint whirring sound as your VR implant spins up. The room focuses into view. You are in a
-                standard laboratory. There is a locked safe in the centre of the room.`,
-                actions: [],
-                // exits: [POWER_REACTOR, STUDY_TABLE, AI_CONSTRUCT, CHEMICAL_STORE, KEY_ROOM],
-                // exits: [game.rooms[1].id, ]
-            },
-
-            {
-                id: "POWER_REACTOR",
-                description: `You walk over to the power reactor.\nThe surrounding area is warm, and a faint orange glow emits from the machine.\nThere is a note stuck on the reactor, and a big red button.`,
-                actions: ["POWER_NOTE", "POWER_BUTTON"],
-                // exits: [MAIN_ROOM],
-            },
-
-            {
-                id: "STUDY_TABLE",
-                description: `The table is littered with notes and drawings, on paper ("How old!" you wonder).\nThere is an iMike standard-issue computer on the table.`,
-                actions: ["STUDY_NOTE", "I_MIKE"],
-                // exits: [MAIN_ROOM],
-            },
-            
-            {
-                id: "AI_CONSTRUCT",
-                description: `You see a female AI, quietly meditating in the corner. `,
-                actions: ["AI_TALK"],
-                // exits: [MAIN_ROOM],
-            },
-
-            {
-                id: "CHEMICAL_STORE",
-                description: `In the storeroom, there's a small vial left behind on the table.`,
-                actions: ["CHEM_READ", "CHEM_SMELL"],
-                // exits: [MAIN_ROOM],
-            },
-
-        ],
-        actions: [
-
-            {
-                roomid: "POWER_REACTOR",
-                titleBtn: "Read note",
-                id_done: false,
-                description: `"Dave told me to clean the reactor, but I haven't been paid for 2 months.\nToo bad, I'm not gonna do anything. Who cares about the stupid jQuery library anyway, it's so old!"
-                \n*You take a small circuit board wedged behind the note. This looks like a clue!*`,
-                
-
-            },
-
-            {
-                roomid2: "POWER_REACTOR",
-                titleBtn2: "Press Button",
                 description: `A siren sounds as the doors of the machine swing open, bathing you in a blinding flash of light.
                 \nYou hear your skin crackling as the radiation reduces you to dust. Game Over.`,
-                // result: DEATH,
             },
-
-            {
-                roomid: "STUDY_TABLE",
-                titleBtn: "Read Notes",
-                id_done: false,
+            STUDY_NOTES : {
+                title: "Read Study Notes",
                 description: `Despite me repeatedly warning them, corporate refuses to put on greater encryption.
                 \nI'm done. I'm supposed to hide the unlocker device but I'll do it next time. - 27 July 2081, Researcher Farhan, Justin, Sylvester, et al.
                 \n*You take the unlocker device and notice it's missing a few parts*`,
-                // result: ADD_CLUE,
             },
-
-            {
-                roomid2: "STUDY_TABLE",
-                titleBtn2: "Turn on iMike",
+            STUDY_IMIKE : {
+                title: "Turn on iMike Computer",
                 description: `As the boot sequence of the computer commences, the police break into the room.
                 \nAiming a raygun at you, they fire a .remove() function in your face as you fade from the world. Game Over.`,
-                // result: DEATH,
             },
-
-            {
-                roomid: "AI_CONSTRUCT",
-                titleBtn: "Talk to AI",
-                id_done: false,
+            AI_TALK : {
+                title: "Talk to AI",
                 description: `"Greetings, Simon, I'm A5HL3Y. I didn't know people still visited here.
                 \nFrom what I can see in your implant, you are not from here. You must be trying to save the world.
                 \nSadly I cannot, as I am stuck here in this computer. Please, take this circuit board. It'll help you with your quest."
                 \n*A5HL3Y hands you a circuit board. This looks like a clue!*`,
-                // result: ADD_CLUE,
             },
-
-            {
-                roomid: "CHEMICAL_STORE",
-                titleBtn: "Read Vial",
-                id_done: false,
+            CHEM_READ : {
+                title: "Read Label on Vial",
                 description: `"IF THEY FORCE YOU TO HAND OVER THE CIRCUIT BOARD, DROP THE CIRCUIT INTO THIS VIAL AND DRINK IT. YOU'LL DIE, BUT SO WILL THE BOARD."
                 \n*You notice a circuit board stuck under the vial. Looks like you found a clue!*`,
-                // result: ADD_CLUE,
             },
-
-            {
-                roomid2: "CHEMICAL_STORE",
-                titleBtn2: "Smell Vial",
+            CHEM_SMELL : {
+                title: "Smell contents of Vial",
                 description: `You uncork the vial and take a whiff. The vial smells strongly of bitter almonds. A bit of the liquid gets on your lips.
                 \nYou get a headache and notice blood dripping from your nostrils, as you crumple to the ground like a wet newspaper and expire. Game over.`,
-                // result: DEATH,
             },
 
-        ]
-        */
+
+        },
+
+        
     }
 
         
 
    
 
-    /**
-     * Reads the clue
-     * 
-     * Step 1: See if the clue has been read
-     * Step 2: If the clue is read, we will prompt the user
-     * Step 3: if the clue is not read, we will display the clue
-     * 
-     * @param {string} actionId 
-     */
+    // CLUE READER FUNCTION
     const readRoomClue = (actionId) => {
-        if (readClues.includes(actionId)) {
-            alert("Sorry, this clue has mysteriously vanished.")
+        if (readClues.includes(actionId)) { 
+            alert("Sorry, you've already read this clue.") 
         } else {
-            //  Display the clue
             alert(game.actions[actionId].description)
             readClues.push(actionId)
             displayClueBar()
         }
     }
 
-    /**
-     * Navigates to a room
-     * Note that it does 
-     * 
-     * @param {string} roomId 
-     */
+    
+
+    // ROOM NAVIGATOR FUNCTION
     const navigateToRoom = (roomId) => {
         currentRoomId = roomId
         displayRoom(currentRoomId)
@@ -237,6 +164,9 @@ $(() => {
 
         // Get the room
         const room = game.rooms[roomId]
+
+        // Display the title, which is stored in currentRoomId
+        $("#title").html(currentRoomId)
         
         // Display the description
         $("#description").html(room.description)
